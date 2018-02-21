76b1c0c5d2362bfd29f66e3b3970ffca39e9f78e
Natasha Wong <natashaylwong@berkeley.edu>
2018-02-20 21:13:01 -0800

    Initial Commit

---
+++ MDB Socials/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MDB Socials
+//
+//  Created by Natasha Wong on 2/20/18.
+//  Copyright © 2018 Natasha Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
