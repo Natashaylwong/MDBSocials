6e4d2616305a423c8ff0213252570e62b80822b8
Natasha Wong <natashaylwong@berkeley.edu>
2018-02-22 19:59:53 -0800

    uh

--- MDB Socials/Controllers/Login/LoginViewController.swift
+++ MDB Socials/Controllers/Login/LoginViewController.swift
@@ -34,10 +34,10 @@ class LoginViewController: UIViewController {
         }
     }
     
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        view.endEditing(true)
-//        super.touchesBegan(touches, with: event)
-//    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
     
     func setupTitle() {
         
